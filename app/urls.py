a34fd69dd5f3dc9c55a94a1285b478aed7768958
Manuel Escalera <manuel.jh.escalera@gmail.com>
2024-04-02 23:48:33 -0300

    agregadas rutas

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('animales', views.animales, name='animales'),
+    path('armas', views.armas, name='armas'),
+    path('construcciones', views.construcciones, name='construcciones'),
+    path('consumibles', views.consumibles, name='consumibles'),
+    path('enemigos', views.enemigos, name='enemigos'),
+    path('flora', views.flora, name='flora'),
+    path('foro', views.forowiki, name='foro'),
+    path('historia', views.historia, name='historia'),
+    path('inicio-sesion', views.inicioSesion, name='inicio sesion'),
+    path('logros', views.logros, name='logros'),
+    path('lugares', views.menuPrincipal, name='lugares'),
+    path('', views.menuPrincipal, name='home'),
+    path('cuenta', views.miCuenta, name='cuenta'),
+    path('recuperar-contrasena', views.recuperarContraseña, name='recuperar contrasena'),
+    path('registro', views.registro, name='registro'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,47 @@
 from django.shortcuts import render
 
-def home(request):
+
+def animales(request):
+    return render(request, 'app/Animales.html')
+
+def armas(request):
+    return render(request, 'app/Armas.html')
+
+def construcciones(request):
+    return render(request, 'app/Construcciones.html')
+
+def consumibles(request):
+    return render(request, 'app/Consumibles.html')
+
+def enemigos(request):
+    return render(request, 'app/Enemigos.html')
+
+def flora(request):
+    return render(request, 'app/Flora.html')
+
+def forowiki(request):
+    return render(request, 'app/forowiki.html')
+
+def historia(request):
+    return render(request, 'app/historia.html')
+
+def inicioSesion(request):
+    return render(request, 'app/inicio_sesion_wiki.html')
+
+def logros(request):
+    return render(request, 'app/logros.html')
+
+def lugares(request):
+    return render(request, 'app/lugarestf.html')
+
+def menuPrincipal(request):
     return render(request, 'app/menuprincipal_wiki.html')
+
+def miCuenta(request):
+    return render(request, 'app/micuentatf.html')
+
+def recuperarContraseña(request):
+    return render(request, 'app/recuperarcontra.html')
+
+def registro(request):
+    return render(request, 'app/registrase_wiki.html')
